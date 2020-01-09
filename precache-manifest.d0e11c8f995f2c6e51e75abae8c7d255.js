ac7f651a6f755a25a086fe63dee8eaf2393a31f2
Benjamina Randrianasolo <minarandria@gmail.com>
2020-01-09 15:02:23 +0300

    Init react app

---
+++ precache-manifest.d0e11c8f995f2c6e51e75abae8c7d255.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cff88549fa50c36751016f3bccf9c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1beb79e6549873b240e5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46dfed47639caeb3e352",
+    "url": "/static/js/2.9f4388f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9f4388f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1beb79e6549873b240e5",
+    "url": "/static/js/main.0fff24de.chunk.js"
+  },
+  {
+    "revision": "50f3c10a873c803cc1cf",
+    "url": "/static/js/runtime-main.31e4fea3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
